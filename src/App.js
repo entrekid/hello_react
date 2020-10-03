16887320cfc32ab3e2d9190ae969336ac559d05c
entrekid <dat.sci.seol@gmail.com>
2020-10-04 02:10:26 +0900

    props vs state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import Content from "./components/Content";
 
  
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB", sub:"World Wide Web"},
+      contents: [
+        {id:1, title:"HTML", desc:"HTML is no language."},
+        {id:2, title:"CSS", desc:"CSS is beautiful"},
+        {id:3, title:"JavaScript", desc:"JS is good"}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub = "world wide web"></Subject>
-        <TOC></TOC>
+        <Subject title= {this.state.subject.title} sub = {this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
